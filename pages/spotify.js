a862eece875e8f8c7b1e62790562205ae74dda06
Khor Zhen Win <khorzhenwin@gmail.com>
2022-12-11 00:38:14 +0800

    [WIP] return authCode to call POST /token

--- pages/spotify.js
+++ pages/spotify.js
@@ -1,19 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "@chakra-ui/react";
+import querystring from "querystring";
+
+function getAuthorizationPath() {
+  const PATH = "https://accounts.spotify.com/authorize";
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const responseType = "code";
+  const redirectUri = encodeURI(process.env.NEXT_PUBLIC_REDIRECT_URI);
+  const scope =
+    "user-read-private user-read-email user-read-playback-state playlist-read-private playlist-read-collaborative user-follow-read user-read-currently-playing user-read-playback-position user-modify-playback-state user-library-read user-library-modify user-top-read user-read-recently-played user-follow-modify";
+
+  return `${PATH}?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&scope=${scope}&show_dialog=true`;
+}
+
+async function getToken(authCode) {
+  const path = "https://accounts.spotify.com/api/token";
+
+  const body = querystring.stringify({
+    grant_type: "authorization_code",
+    code: authCode,
+    redirect_uri: encodeURI(process.env.NEXT_PUBLIC_REDIRECT_URI),
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  });
+
+  const header = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization:
+      "Basic " +
+      Buffer.from(
+        process.env.NEXT_PUBLIC_CLIENT_ID +
+          ":" +
+          process.env.NEXT_PUBLIC_CLIENT_SECRET
+      ).toString("base64"),
+  };
+
+  const response = await fetch(path, {
+    method: "POST",
+    headers: header,
+    body,
+  });
+  console.log(response);
+  return response;
+}
 
 export default function Spotify() {
   const [url, setUrl] = useState(null);
   const [authCode, setAuthCode] = useState(null);
-
-  const AUTHORIZE = "https://accounts.spotify.com/authorize";
-  let urlPath = AUTHORIZE;
-  urlPath += "?client_id=" + process.env.NEXT_PUBLIC_CLIENT_ID;
-  urlPath += "&response_type=code";
-  urlPath +=
-    "&redirect_uri=" + encodeURIComponent(process.env.NEXT_PUBLIC_REDIRECT_URI);
-  urlPath +=
-    "&scope=user-read-private user-read-email user-read-playback-state playlist-read-private playlist-read-collaborative user-follow-read user-read-currently-playing user-read-playback-position user-modify-playback-state user-library-read user-library-modify user-top-read user-read-recently-played user-follow-modify";
-  urlPath += "&show_dialog=true";
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     if (url) window.location.href = url;
@@ -27,7 +61,11 @@ export default function Spotify() {
     const urlParams = new URLSearchParams(queryString);
     if (urlParams.has("code")) {
       const code = urlParams.get("code");
-      if (code) setAuthCode(urlParams.get("code"));
+      if (code) {
+        setAuthCode(urlParams.get("code"));
+        setToken(getToken(code).toString());
+        console.log(token);
+      }
     }
   }, [authCode]);
 
@@ -36,8 +74,9 @@ export default function Spotify() {
   // localStorage.setItem("redirect_uri", process.env.REDIRECT_URI);
   return (
     <>
-      <Button onClick={() => setUrl(urlPath)}>Login</Button>
+      <Button onClick={() => setUrl(getAuthorizationPath())}>Login</Button>
       <p>{authCode}</p>
+      <p>{token}</p>
     </>
   );
 }
